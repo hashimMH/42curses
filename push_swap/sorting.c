f075ca4751e8a025674ce946ff032bb7ffa01f76
hashimMH <hashimstar25@gmail.com>
2023-01-20 19:30:12 +0400

    min num

--- push_swap/sorting.c
+++ push_swap/sorting.c
@@ -6,37 +6,36 @@
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 19:37:46 by hmohamed          #+#    #+#             */
-/*   Updated: 2023/01/17 18:39:20 by hmohamed         ###   ########.fr       */
+/*   Updated: 2023/01/20 19:28:18 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void sort(t_list **a)
+void	sort(t_list **a)
 {
-	t_list *b;
-	t_list *tm;
+	t_list	*b;
+	t_list	*tm;
+	int i;
 
 	b = NULL;
 	tm = *a;
+	i = ft_lstsize(tm);
+	if (i == 2 && !is_sorted(tm, b))
+		sa(&tm);
+	else if (i == 3 && !is_sorted(tm, b))
+		sort_three(&tm);
+	else if (i == 4 && !is_sorted(tm, b))
+		sort_four(&tm, &b);
+	else if (i == 5 && !is_sorted(tm, b))
+		sort_five(&tm, &b);
 	if (is_sorted(tm, b))
-		printf("sorted\n");
-	else
-		printf("not_sorted");
-	pb(&tm, &b);
-	pb(&tm, &b);
-	pa(&tm, &b);
-	 if (!a)
-	 	write(2, "error", 6);
+		 printf("sooorted");
 	while (tm)
 	{
-		printf(" list val: %d", (tm->content));
+		// printf("num : %d ", tm->content);
 		tm = tm->next;
 	}
-	while (b)
-	{
-		printf(" \nlist val of b: %d", (b->content));
-		b = b->next;
-	}
-	printf("\n end of func");
+	
+	// printf("\n end of func");
 }
