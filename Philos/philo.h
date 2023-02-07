2aca860f7bf6abbb1065ea9f2e1bde7f1cf8e409
hashimMH <hashimstar25@gmail.com>
2023-02-07 18:35:07 +0400

    philo

---
+++ Philos/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 18:32:49 by hmohamed          #+#    #+#             */
+/*   Updated: 2023/02/07 18:33:59 by hmohamed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+#endif
